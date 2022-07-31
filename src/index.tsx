5ef4a7a231f0ff349e20f081f754ccb6d6e446b4
Khanh Nguyen <khanh.nguyen@techupcorp.com>
2022-07-31 09:42:42 +0700

    feat: add sentry to tracking error

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,22 @@
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+Sentry.init({
+  dsn: 'https://72e3a3cd236c49078b8baeb06f9af4f9@o1340985.ingest.sentry.io/6614049',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
     <App />
