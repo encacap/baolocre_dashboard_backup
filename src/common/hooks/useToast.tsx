72a3d7fa3c59d6d706fc50d1f4dce75fd7fd0136
Khanh Nguyen <khanh.nguyen@techupcorp.com>
2022-08-16 22:26:54 +0700

    feat: improve useToast

--- src/common/hooks/useToast.tsx
+++ src/common/hooks/useToast.tsx
@@ -1,18 +1,51 @@
-import { ToastOptions as ChakraToastOptions, useToast as chakraUseToast } from '@chakra-ui/react';
+import {
+  ToastOptions as ChakraToastOptions,
+  useToast as chakraUseToast,
+  UseToastOptions,
+} from '@chakra-ui/react';
+import { AlertProps } from '../../app/types/common';
 import ToastMessage from '../components/toast/ToastMessage';
 
-type ToastOptions = Pick<ChakraToastOptions, 'duration' | 'id' | 'requestClose'>;
+type ToastOptions = Partial<Pick<ChakraToastOptions, 'duration' | 'id' | 'requestClose'>>;
+
+interface ShowToastOptions extends ToastOptions {
+  message: string;
+  description?: string;
+  status: AlertProps['status'];
+}
 
 const useToast = () => {
   const chakraToast = chakraUseToast();
 
+  const showToast = (options: ShowToastOptions) => {
+    const { id, message, description, status } = options;
+    const showToastOptions: UseToastOptions = {
+      ...options,
+      containerStyle: {
+        padding: 2,
+      },
+      position: 'top-right',
+      render: () => <ToastMessage message={message} description={description} status={status} />,
+    };
+
+    if (id) {
+      if (!chakraToast.isActive(id)) {
+        return chakraToast(showToastOptions);
+      }
+      return chakraToast.update(id, showToastOptions);
+    }
+
+    return id || undefined;
+  };
+
   return {
     ...chakraToast,
     success: (message: string, description?: string, options?: ToastOptions) =>
-      chakraToast({
+      showToast({
         ...options,
-        position: 'top-right',
-        render: () => <ToastMessage message={message} description={description} status="success" />,
+        message,
+        description,
+        status: 'success',
       }),
   };
 };
